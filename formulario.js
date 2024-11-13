0422656c7a7c892f0d6d85254eb67bc479096d0f
viniciussilva2504 <vinicius.silva2504@gmail.com>
2024-11-13 18:55:22 +0000

    alteração js formulario

--- formulario.js
+++ formulario.js
@@ -2,15 +2,19 @@ $(document).ready(function () {
     $('#telefone').mask('(+351) 000 000 000', {
         placeholder: '(+351) 000 000 000'
     });
+
     $('#nome').on('input', function () {
         let valor = $(this).val();
-        let regexNome = /^[A-Za-zÀ-ÿ\s]+$/;
-        if (valor.length > 50 || !regexNome.test(valor)) {
-            $(this).get(0).setCustomValidity('Nome deve conter apenas letras e até 50 caracteres.');
+        let regexNome = /^[A-Za-zÀ-ÿ\s]+$/; 
+        if (valor.length > 80) {
+            $(this).get(0).setCustomValidity('O nome não pode ter mais de 80 caracteres.');
+        } else if (valor.length <= 50 || !regexNome.test(valor)) {
+            $(this).get(0).setCustomValidity('Nome deve conter apenas letras e até 80 caracteres.');
         } else {
-            $(this).get(0).setCustomValidity('');
+            $(this).get(0).setCustomValidity(''); 
         }
     });
+
     $('#email').on('input', function () {
         let valor = $(this).val();
         if (valor.length > 100) {
@@ -21,6 +25,7 @@ $(document).ready(function () {
             $(this).get(0).setCustomValidity('');
         }
     });
+
     $('#mensagem').on('input', function () {
         let valor = $(this).val();
         if (valor.length > 200) {
@@ -29,6 +34,7 @@ $(document).ready(function () {
             $(this).get(0).setCustomValidity('');
         }
     });
+
     $('#formulario').on('submit', function (e) {
         if (!this.checkValidity()) {
             e.preventDefault();
